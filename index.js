9f9eb896c388bc7bb6c87c0c95ef06c4deebc580
Charles Cusson <charlescusson@gmail.com>
2020-04-04 18:18:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function theBeatlesPlay(musi,inst){
 
 }
 
-/*function johnLennonFacts(facts){
+function johnLennonFacts(facts){
 var facts2 = []
   let i=0
-  while(i<=facts.length){
+  while(i<facts.length){
     facts2.push(facts[i] + "!!!")
   } return facts2
 }
-*/
+
