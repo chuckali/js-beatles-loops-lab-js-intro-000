29f2f9c7eef10f625745c5a32d81a64eb68b81de
Charles Cusson <charlescusson@gmail.com>
2020-04-04 18:15:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musi,inst){
+  var who  = [];
+  for(let i=0; i<4; i++){
+    who.push(`${musi[i]} plays ${inst[i]}`)
+  }
+  return who
+
+}
+
+function johnLennonFacts(facts){
+var facts2 = []
+  let i=0
+  while(i=<facts.length){
+    facts2.push(facts[i] + "!!!")
+  } return facts2
+}
